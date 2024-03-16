6d19690aa143e43f0f1f898214bb9382520c5195
Abdullah <.alhumidi.Ab@gmail.com>
2024-03-17 01:58:50 +0300

    adding the files

---
+++ BlogWebsite/blog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from django.http import HttpRequest, HttpResponse
+from django.utils import timezone  # Import timezone module
+from .forms import PostForm  # Make sure to import PostForm
+
+def home(request):
+    posts = Post.objects.filter(is_published=True).order_by('-published_at')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html',context)
+
+
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'blog/add_post.html', {'form': form})
