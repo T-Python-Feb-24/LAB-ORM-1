e5ce360efc2d338e605ca165f38f5daaa4ce9fd7
Hammam <hajeddawi@gmail.com>
2024-03-18 04:11:44 +0300

    midified the code except for the 'category' the code is there commented

--- BlogWebsite/main/urls.py
+++ BlogWebsite/main/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 app_name = "main"
 
-urlpatterns = [
+urlpatterns  = [
     path("", views.index_view, name="index_view"),
-    path("new/blog/", views.add_blog_view, name="add_blog_view")
+    path("post/add/", views.add_post_view, name="add_post_view"),
+    path("post/detail/<post_id>/", views.post_detail_view, name="post_detail_view"),
+    path("post/update/<post_id>/", views.update_post_view, name="update_post_view"),
+    path("post/delete/<post_id>/", views.delete_post_view, name="delete_post_view")
 ]
--- BlogWebsite/main/views.py
+++ BlogWebsite/main/views.py
@@ -1,36 +1,77 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
-from .models import Blog
-from django.utils import timezone
+from .models import Post#, MyChoices
 # Create your views here.
 
 def index_view(request:HttpRequest):
 
     #get all entries insisde database
-    blogs = Blog.objects.all()
+    posts = Post.objects.all()
     context = {
-        "blogs" : blogs
+        "posts" : posts
     }
 
     return render(request, "main/index.html", context)
 
-def add_blog_view(request:HttpRequest):
 
-    #adding a new entry
+def add_post_view(request: HttpRequest):
+    #categories = MyChoices.choices ---  there is an error here i dont know how to solve it
+ 
+    if request.method == 'POST':
+        try:
+            new_post = Post(
+                title=request.POST["title"], 
+                content=request.POST["content"],
+                # category=request.POST["category"], 
+                is_published=request.POST.get("is_published", False), 
+                poster=request.FILES["poster"]
+            )
+            new_post.save()
+            return redirect("main:index_view")
+        except Exception as e:
+            print(e)
+
+    return render(request, "main/add_post.html")
+
+
+def post_detail_view(request:HttpRequest, post_id):
+
+    try:
+        #getting a  post detail
+        post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        return render(request, 'main/not_exist.html')
+    except Exception as e:
+        print(e)
+    return render(request, "main/post_detail.html", {"post" : post})
+
+def update_post_view(request:HttpRequest, post_id):
+
+    post = Post.objects.get(pk=post_id)
+
     if request.method == "POST":
-        # Get the value of is_published from the form
-        is_published_value = request.POST.get("is_published")
+        try:
+            post.title = request.POST["title"]
+            post.content = request.POST["content"]
+            # post.category = request.POST["category"]
+            post.is_published = request.POST.get("is_published", False)
+            post.save()
+            return redirect("main:post_detail_view", post_id=post.id)
+        except Exception as e:
+            print(e)
 
-        # Convert "on" to True, otherwise set it to False
-        is_published = is_published_value == "on"
+    
+    return render(request, 'main/update_post.html', {"post" : post})
+
+def delete_post_view(request:HttpRequest, post_id):
+
+    try:
+        post = Post.objects.get(pk=post_id)
+        post.delete()
+    except Exception as e:
+        print(e)
+    
+
+    return redirect("main:index_view")
+
         
-        new_blog = Blog(
-            title=request.POST["title"], 
-            content=request.POST["content"], 
-            is_published=is_published, 
-            published_at=request.POST.get("published_at") or timezone.now()
-        )
-        new_blog.save()
-        return redirect("main:index_view")
-
-    return render(request, "main/add_blog.html")
