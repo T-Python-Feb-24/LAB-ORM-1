f2d46f092f1949e2dfc03190f129661c17a266fd
Fai <faiabdulla123@gmail.com>
2024-03-18 04:50:42 +0300

    LAB-ORM-1

--- BlogWebsite/Website/models.py
+++ BlogWebsite/Website/models.py
@@ -6,4 +6,15 @@ class BlogWebsite(models.Model):
     title = models.CharField(max_length=2024)
     content = models.TextField()
     is_published = models.BooleanField()
-    published_at = models.DateTimeField()
+    published_at = models.DateTimeField(auto_now_add=True)
+    poster = models.ImageField(upload_to="images/", default="images/default.jpeg")
+
+class Category(models.TextChoices):
+    GENERAL = 'General'
+    SCIENCE = 'Science'
+    CULTURE = 'Culture'
+    FOOD = 'Food'
+    TECH = 'Tech'
+    FASHION = 'Fashion'
+
+    category_choices = models.CharField(max_length=10, choices=Category.choices, default=Category.GENERAL)
--- BlogWebsite/Website/views.py
+++ BlogWebsite/Website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse ,Http404
 from .models import BlogWebsite
 
 
@@ -24,3 +24,39 @@ def add_post(request: HttpRequest):
         new_post.save()
         return redirect("Website:index")
     return render(request, "Website/add_post.html")
+
+
+def post_detail(reqest:HttpRequest,post_id):
+    
+    post= BlogWebsite.objects.get(pk=post_id)
+    
+    return render(reqest, "Website/post_detail.html",{"post": post})
+
+
+def update_post(request:HttpRequest, post_id):
+    post=BlogWebsite.objects.get(pk=post_id)
+    if request.method == "POST":
+        try:
+            post.title = request.POST["title"]
+            post.content = request.POST["content"]
+            post.is_published = request.POST.get("is_published", False)
+            post.save()
+            return redirect("Website/post_detail.html", post_id=post.id)
+        except Exception as e:
+            print(e)
+    return render(request,'Website/post_detail.html',{"post":post})
+
+
+def delete_post(request:HttpRequest, post_id):
+
+    try:
+        post =BlogWebsite.objects.get(pk=post_id)
+        post.delete()
+    except Exception as e:
+        print(e)
+    
+
+    return redirect("Website:index")
+
+def view_404(request, exception):
+    return render(request, '404.html')
