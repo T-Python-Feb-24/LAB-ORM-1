2e776b5bae2f0eecdfa3fc72effec5551770ee07
Abdullah <.alhumidi.Ab@gmail.com>
2024-03-18 03:29:37 +0300

    adding

--- BlogWebsite/blog/urls.py
+++ BlogWebsite/blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_post, name='add_post'),
+    path('post/<int:pk>/update/', views.post_update, name='post_update'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
--- BlogWebsite/blog/views.py
+++ BlogWebsite/blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Post
 from django.http import HttpRequest, HttpResponse
 from django.utils import timezone  
 from .forms import PostForm  
+from .models import Post
 
 def home(request):
     posts = Post.objects.filter(is_published=True).order_by('-published_at')
@@ -21,3 +22,25 @@ def add_post(request):
     else:
         form = PostForm()
     return render(request, 'blog/add_post.html', {'form': form})
+#################################################################
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')
+    return render(request, 'blog/post_delete_confirm.html', {'post': post})
