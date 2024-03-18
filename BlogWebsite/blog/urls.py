06dc91e63676476705918f412fa3498089584f9a
AsmaaFelemban <Asmaakf@hotmail.com>
2024-03-18 05:47:07 +0300

    LAB-ORM-1 UPDATED

--- BlogWebsite/blog/urls.py
+++ BlogWebsite/blog/urls.py
@@ -6,4 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.home_page, name = 'home_page'),
     path('new/post', views.add_post_page, name = 'add_post_page'),
+    path("post/detail/<post_id>/", views.post_detail_page, name="post_detail_page"),
+      path("post/update/<post_id>/", views.update_post_page, name="update_post_page"),
+    path("post/delete/<post_id>/", views.delete_post_page, name="delete_post_page"),
 ]
--- BlogWebsite/blog/views.py
+++ BlogWebsite/blog/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from .models import Post
@@ -8,18 +9,67 @@ def home_page(request:HttpRequest):
     context = {
         "posts" : posts
     }
-    return render(request, 'blog/home_page.html', context)
+    return render(request, "blog/home_page.html", context)
+
 
 def add_post_page(request: HttpRequest):
     
     if request.method =="POST":
-        new_post = Post(
-            title = request.POST["title"], 
-            content = request.POST["content"],  
-            published_at = request.POST["published_at"], 
-            is_published = request.POST.get("is_published")
-        )
-        new_post.save()
+        try:
+            new_post = Post(
+                title = request.POST["title"], 
+                content = request.POST["content"],  
+                is_published = request.POST.get("is_published", False),
+                poster=request.FILES["poster"]
+            )
+            new_post.save()
+        except Exception as e:
+            print(e)
         return redirect("blog:home_page")
     
     return render(request, "blog/add_post_page.html")
+
+
+def post_detail_page(request:HttpRequest, post_id):
+    
+    try:
+        #getting a  post detail
+        post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        post = None
+    except Exception as e:
+        print(e)
+
+    return render(request, "blog/post_detail_page.html", {"post" : post})
+
+def update_post_page(request:HttpRequest, post_id):
+    post = Post.objects.get(pk=post_id)
+
+    if request.method == "POST":
+        try:
+            if len(request.FILES) != 0:
+                if len(post.poster) > 0:
+                    os.remove(post.poster.path)
+                post.poster = request.FILES["poster"]
+            post.title = request.POST["title"]
+            post.content = request.POST["content"]
+            post.is_published = request.POST.get("is_published", False)
+            post.save()
+
+            return redirect("blog:post_detail_page", post_id=post.id)
+        except Exception as e:
+            print(e)
+
+    
+    return render(request, 'blog/update_post_page.html', {"post" : post})
+
+def delete_post_page(request:HttpRequest, post_id):
+
+    try:
+        post = Post.objects.get(pk=post_id)
+        post.delete()
+    except Exception as e:
+        print(e)
+    
+
+    return redirect("blog:home_page")
