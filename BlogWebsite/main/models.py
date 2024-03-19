ade81a5be36523736d215208779a2907ae704487
Fai <faiabdulla123@gmail.com>
2024-03-19 04:06:11 +0300

    Lab-ORM-1

---
+++ BlogWebsite/main/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+
+    #catergories choices
+    categories = models.TextChoices("Category", ["General", "Tech", "Science", "Fashion"])
+
+    title = models.CharField(max_length=2048)
+    content = models.TextField()
+    is_published = models.BooleanField()
+    published_at = models.DateTimeField(auto_now_add=True)
+    poster = models.ImageField(upload_to="images/", default="images/default.jpeg")
+    category = models.CharField(max_length=64, choices=categories.choices)
