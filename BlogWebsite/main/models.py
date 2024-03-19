3505a944d8dfa8b8a89f2bffa5bab8c50ac9b2ed
Salemhu <salem.hussain.alsalem@gmail.com>
2024-03-19 11:24:19 +0300

    completed.

--- BlogWebsite/main/models.py
+++ BlogWebsite/main/models.py
@@ -4,9 +4,12 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Post(models.Model):
+
+    categories = models.TextChoices("Category", ["General",  "Science", "Culture", "Food", "Tech", "Fashion"])
     
     title = models.CharField(max_length=2048)
     content = models.TextField()
     is_published = models.BooleanField()
     published_at = models.DateTimeField(auto_now_add=True)
-    poster = models.ImageField(upload_to="images/", default="images/default.jpeg")
+    poster = models.ImageField(upload_to="images/", default="images/def.jpg")
+    category = models.CharField(max_length=30, choices=categories.choices)
--- BlogWebsite/main/views.py
+++ BlogWebsite/main/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from datetime import date, timedelta
 # Create your views here.
 from .models import Post
 
 
 def index_view(request: HttpRequest):
 
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-published_at')[0:3]
 
     return render(request, "main/index.html", {"posts" : posts})
 
@@ -18,24 +19,22 @@ def add_post_view(request: HttpRequest):
             new_post = Post(title=request.POST["title"], 
                             content=request.POST["content"], 
                             is_published=request.POST.get("is_published", False), 
-                            poster=request.FILES["poster"],
-                            )
+                            category= request.POST["category"],  
+                            poster=request.FILES["poster"])
             new_post.save()
+            return redirect("main:index_view")
         except Exception as e:
             print(e)
 
-    return render(request, "main/add_post.html")
-
-
+    return render(request, "main/add_post.html", {"categories" : Post.categories.choices})
 
 
 def post_detail_view(request:HttpRequest, post_id):
 
     try:
-        #getting a  post detail
         post = Post.objects.get(pk=post_id)
     except Post.DoesNotExist:
-        post = None
+            return render(request,"main/not_found.html")
     except Exception as e:
         print(e)
 
@@ -49,17 +48,19 @@ def update_post_view(request:HttpRequest, post_id):
 
     if request.method == "POST":
         try:
+            
             post.title = request.POST["title"]
             post.content = request.POST["content"]
             post.is_published = request.POST.get("is_published", False)
-            post.poster = request.POST["poster"]
+            post.category = request.POST["category"]
+            post.poster = request.FILES.get("poster", post.poster)
             post.save()
             return redirect("main:post_detail_view", post_id=post.id)
         except Exception as e:
             print(e)
 
     
-    return render(request, 'main/update_post.html', {"post" : post})
+    return render(request, 'main/update_post.html', {"post" : post, "categories" : Post.categories.choices})
 
 
 def delete_post_view(request:HttpRequest, post_id):
@@ -67,9 +68,47 @@ def delete_post_view(request:HttpRequest, post_id):
     try:
         post = Post.objects.get(pk=post_id)
         post.delete()
+    except Post.DoesNotExist:
+        return render(request, "main/not_found.html")
     except Exception as e:
         print(e)
     
 
     return redirect("main:index_view")
 
+
+def all_posts_view(request: HttpRequest):
+
+    
+    if "cat" in request.GET:
+        posts = Post.objects.filter(category=request.GET["cat"])
+    else:
+        posts = Post.objects.all()
+    
+    limit = 3
+    pages_count = [str(n) for n in range(1, round(posts.count()/limit)+1)] 
+    start = (int(request.GET.get("page", 1))-1)*limit
+    end = (start)+limit
+
+    print(list(pages_count))
+
+
+    posts = posts[start:end]
+
+
+    return render(request, "main/all_posts.html", {"posts" : posts, "categories" : Post.categories.choices, "pages_count":pages_count})
+
+
+def posts_search_view(request:HttpRequest):
+    posts = []
+
+    if "search" in request.GET:
+        posts = Post.objects.filter(title__contains=request.GET["search"])
+
+    if "date" in request.GET and len(request.GET["date"]) > 4:
+        first_date = date.fromisoformat(request.GET["date"])
+        end_date = first_date + timedelta(days=1)
+        posts = posts.filter(published_at__gte=first_date, published_at__lt=end_date)
+
+
+    return render(request, "main/search_page.html", {"posts" : posts})
